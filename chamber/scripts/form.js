f34f6917548d52505d80a4f13391d1ee269681c3
morpho2005 <digitalchristian05@gmail.com>
2025-03-29 18:31:59 -0600

    commit

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -1,8 +1,24 @@
 const regex = /[^a-z0-9\s-]/gi;
 const title = document.querySelector('#title')
 const time = document.querySelector('#time');
+const date = new Date();
 
-time.value = `${new Date().getHours()}:${new Date().getMinutes()}`;
+if (date.getMinutes()<10){
+    let minutes = `0${date.getMinutes()}`
+    if (date.getHours()<10){
+        let hours = `0${date.getHours()}`
+        time.value = `${hours}:${minutes}`;
+    } else {
+        time.value = `${date.getHours()}:${minutes}`;
+    }
+} else {
+    if (date.getHours()<10){
+        let hours = `0${date.getHours()}`
+        time.value = `${hours}:${date.getMinutes()}`;
+    } else {
+        time.value = `${date.getHours()}:${date.getMinutes()}`;
+    }
+}
 
 console.log(time.value);
 
